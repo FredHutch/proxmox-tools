00eababc7aa7c2df0b92720127b133602d54af3c
dirkpetersen <petersen@fhcrc.org>
2016-11-13 12:15:35 -0800

    version 1.0

--- pypi-push.sh
+++ pypi-push.sh
@@ -10,8 +10,8 @@ git add README.rst
 git commit -a -m "version ${version}"
 git tag ${version} -m "tag for PyPI"
 git push --tags origin master
-python3 setup.py register -r pypitest
-python3 setup.py sdist upload -r pypitest
+python3 setup.py register -r pypi
+python3 setup.py sdist upload -r pypi
 
 echo "  Done! Occasionally you may want to remove older tags:"
 echo "git tag 1.2.3 -d"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "0.9.6"
+__version__ = "1.0"
 
 #try:
 #    from pypandoc import convert
